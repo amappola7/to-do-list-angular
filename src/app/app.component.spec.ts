31341a7621579647ce31f959982041bab0909519
Amappola <anam.porrasp@gmail.com>
2023-12-19 08:33:39 -0500

    Default settings

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,12 +14,6 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'view-child-exercise'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('view-child-exercise');
-  });
-
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TasksComponent } from './components/tasks/tasks.component';
 import { TasksListComponent } from './components/tasks-list/tasks-list.component';
 import { TaskFormComponent } from './components/tasks-list/components/task-form/task-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TasksComponent,
     TasksListComponent,
     TaskFormComponent
   ],
