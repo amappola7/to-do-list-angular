7fd037b00da7ec336ba6d97ff1df7994d0da9ac5
Amappola <anam.porrasp@gmail.com>
2023-12-19 09:19:43 -0500

    Displaying tasks

--- src/app/pages/tasks/components/tasks-list/tasks-list.component.ts
+++ src/app/pages/tasks/components/tasks-list/tasks-list.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Task } from 'src/app/models/task';
+import { TaskService } from 'src/app/services/task/task.service';
 
 @Component({
   selector: 'app-tasks-list',
   templateUrl: './tasks-list.component.html',
   styleUrls: ['./tasks-list.component.scss']
 })
-export class TasksListComponent {
+export class TasksListComponent implements OnInit{
   tasksList: Task[] = [];
+
+  constructor(
+    private taskService: TaskService
+  ) {}
+
+  ngOnInit(): void {
+    this.taskService.getTasks()
+    .subscribe((tasks) => this.tasksList = tasks);
+  }
 }
